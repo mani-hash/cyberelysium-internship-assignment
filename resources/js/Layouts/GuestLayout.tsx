eaf9caf82f0e82e781a9163951aa4757077046e6
mani-hash <manimehalan400@gmail.com>
2024-03-26 18:19:12 +0530

    Added navbar to guest layout

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -4,16 +4,43 @@ import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+        <div className="min-h-screen flex flex-col pt-0 bg-gray-100">
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {children}
+            {/* Navbar */}
+            <GuestNavBar />
+
+            {/* Content */}
+            <div className="flex flex-col justify-center items-center flex-grow">
+                <div>
+                    <Link href="/">
+                        <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                    </Link>
+                </div>
+
+                <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                    {children}
+                </div>
             </div>
+        </div>
+    );
+}
+
+function GuestNavBar() {
+    return (
+        <div className="w-full p-6 text-end">
+            <Link
+                href={route('login')}
+                className="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-500"
+            >
+                Log in
+            </Link>
+
+            <Link
+                href={route('register')}
+                className="ms-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-500"
+            >
+                Register
+            </Link>
         </div>
     );
 }
