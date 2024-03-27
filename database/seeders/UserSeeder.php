f968eab0fb480ee34989755d19379bfcf55bc67a
mani-hash <manimehalan400@gmail.com>
2024-03-27 15:21:43 +0530

    added user seeder info

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table("users")->insert([
+            [
+                "name" => "mani123",
+                "email" => "mani@gmail.com",
+                "password" => Hash::make("qwerty123"),
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+                "updated_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ]
+        ]);
+    }
+}
