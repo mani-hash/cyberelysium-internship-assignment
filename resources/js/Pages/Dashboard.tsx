21ebcc9cc5e663c67638f8ac649db2fae9c59c5a
mani-hash <manimehalan400@gmail.com>
2024-03-27 14:49:06 +0530

    Modified dashboard to include panel

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
+import Panel from './Dashboard/Panel';
 
 export default function Dashboard({ auth }: PageProps) {
     return (
@@ -13,7 +14,10 @@ export default function Dashboard({ auth }: PageProps) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <div>
+                            <Panel className="p-6 text-gray-900" />
+                        </div>
+                        {/* <div className="p-6 text-gray-900">You're logged in!</div> */}
                     </div>
                 </div>
             </div>
