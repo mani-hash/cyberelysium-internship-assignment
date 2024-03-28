0a0c21effed6967d0ee6210c37c68af7f362eeaa
mani-hash <manimehalan400@gmail.com>
2024-03-28 16:38:36 +0530

    created view student modal

---
+++ resources/js/Pages/Dashboard/Partials/ViewStudent.tsx
@@ -0,0 +1,28 @@
+import Modal from "@/Components/Modal";
+
+export default function ViewStudent(
+    { image, show, onClose }: 
+    { image: string|null, show: boolean, onClose: CallableFunction}
+) {
+
+    function sanitizeImageUrl(image_url: string) {
+        return image_url.replace('public', '/storage');
+    }
+    
+    return (
+        <Modal show={show} onClose={onClose}>
+            <div className="px-5 py-3 mx-auto">
+                <h5 className="text-center font-extrabold text-xl underline underline-offset-2">Student Image</h5>
+                <div className="p-3 flex flex-row justify-center">
+                    {image !== null ? 
+                        <img className="w-[300px] h-[300px] object-contain" src={sanitizeImageUrl(image)} alt="" /> 
+                        : 
+                        <p>No image is available!</p>}
+                    
+
+                </div>
+            </div>
+            {/* {image === null ? 'error' : image} */}
+        </Modal>
+    );
+}
