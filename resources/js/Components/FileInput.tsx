a4633b3646d474a21dafe3b21acfc332bd4a60d7
mani-hash <manimehalan400@gmail.com>
2024-03-28 12:00:51 +0530

    included ref for file input component

--- resources/js/Components/FileInput.tsx
+++ resources/js/Components/FileInput.tsx
@@ -1,16 +1,19 @@
-import { InputHTMLAttributes } from "react";
+import { ForwardedRef, InputHTMLAttributes, forwardRef, useRef } from "react";
 
-export default function FileInput({id, className, ...props} : InputHTMLAttributes<HTMLInputElement>) {
-    return (
-        <div className={className}>
-          
-          <input
-            {...props}
-            className={`relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none`}
-            type="file"
-        />
+export default forwardRef(function FileInput({className, ...props} : InputHTMLAttributes<HTMLInputElement>, ref : ForwardedRef<HTMLInputElement>) {
+  
+  return (
+      <div className={className}>
         
-      </div>
-            
-    );
-}
+        <input
+          ref={ref}
+          {...props}
+          className={`relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none`}
+          type="file"
+      />
+      
+    </div>
+          
+  );
+});
+
