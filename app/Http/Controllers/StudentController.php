d37181a6f0f08e62e0996f9fd8f6b7acb15c4f2e
mani-hash <manimehalan400@gmail.com>
2024-03-28 12:00:12 +0530

    added index, create and store methods for resource students

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,33 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StudentRequest;
 use App\Models\Student;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class StudentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        $students = Student::select("id", "name", "age", "status", "image")->get();
+
+        return Inertia::render('Dashboard/ViewStudents', [
+            'students' => $students,
+        ]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): Response
     {
-        //
+        return Inertia::render('Dashboard/AddStudent');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StudentRequest $request): RedirectResponse
     {
-        //
+        $path = $this->storeImage($request->file("image"));
+
+        $student = new Student();
+        $student->name = $request->input('name');
+        $student->age = $request->input('age');
+        $student->status = $request->input('status');
+        $student->image = $path;
+        $student->save();
+
+    
+        return redirect()->route('dashboard.students.create');
     }
 
     /**
@@ -61,5 +82,16 @@ class StudentController extends Controller
     public function destroy(Student $student)
     {
         //
+    }
+
+    // Store the image
+    private function storeImage($file): string|null
+    {
+        if (!$file) {
+            return null;
+        }
+
+        $path = Storage::putFile('public/image', $file);
+        return $path;
     }
 }
